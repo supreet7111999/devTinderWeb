86f2ff1a0331bbbadf3ee99cd40d97e4d2b84472
Supreet <supreet7111999@gmail.com>
2025-07-16 19:00:04 +0530

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import Navbar from "./components/Navbar"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+// import Navbar from "./components/Navbar"
+import Main from "./pages/Main"
+import Login from "./pages/Login"
+import Profile from "./pages/Profile"
+import Signup from "./pages/Signup"
+import Feed from "./pages/Feed"
 
 
 function App() {
 
   return (
     <>
-     <Navbar/>
+     <BrowserRouter>
+       <Routes>
+        <Route path="/" element={<Main/>}>
+          <Route path="/" element={<Login/>} />
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/feed" element={<Feed/>}/>
+          <Route path="/signup" element={<Signup/>} />
+        </Route>
+       </Routes>
+     </BrowserRouter>
     </>
   )
 }
