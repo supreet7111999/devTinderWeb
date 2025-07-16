583f5a70e0ebde89933ea943c8e43382ba325555
Supreet <supreet7111999@gmail.com>
2025-07-16 20:03:33 +0530

    Supreet|Added Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  
+  const [userData,setUserData]=useState({
+    "email":'',
+    "password":'',
+  });
+
+  const handleChange=(e)=>{
+    const {name,value}=e.target;
+    setUserData({...userData,[name]:value});
+  };
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    //logic
+    console.log(userData);
+  }
   return (
-    <div>Login</div>
+    <div className='flex justify-center my-10'>
+      <div className="card bg-base-100 w-96 shadow-sm ">
+  <figure>
+    <img
+      src="https://plus.unsplash.com/premium_photo-1720192861639-1524439fc166?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bG9naW58ZW58MHx8MHx8fDA%3D"
+      alt="Login" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      Login Please
+    </h2>
+    <div>
+       <fieldset className="fieldset">
+       <legend className="fieldset-legend">Email</legend>
+       <input 
+         type="text" 
+         className="input" 
+         placeholder="Type here" 
+         name='email'
+         onChange={handleChange}
+       />
+        
+       </fieldset>
+    </div>
+    <div>
+      <fieldset className="fieldset">
+       <legend className="fieldset-legend">Password</legend>
+       <input 
+         type="password" 
+         className="input" 
+         placeholder="Type here" 
+         name='password'
+         onChange={handleChange}
+       />
+       </fieldset>
+    </div>
+    <button 
+      className="btn btn-wide mx-8"
+      onClick={handleSubmit}
+      >Login</button>
+  </div>
+</div>
+    </div>
   )
 }
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,13 +5,14 @@ import Footer from '../components/Footer'
 
 const Main = () => {
   return (
-    <>
-     <Navbar/>
-     <Outlet/>
-     <Footer/>
-    </>
-    
+    <div className='flex flex-col h-screen'>
+       <Navbar/>
+        <div className='flex-1 overflow-y-auto pb-16'>
+          <Outlet/>
+        </div>
+        <Footer className='fixed bottom-0 left-0 w-full h-[10px] bg-gray-800 text-white'/>
+    </div>
   )
 }
 
-export default Main
+export default Main;
